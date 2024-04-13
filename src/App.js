b32d9d687eb3b0ea810d0b89a61bd5315eebd895
shreeya <pandeyshreeya3@gmail.com>
2024-04-13 23:03:56 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+import styled from "styled-components"; 
+import Tracker from "./components/Tracker"; 
+import GlobalStyles from "./globalStyles"; 
+import moneysImage from "./photos/moneys.png";
+  
+const Main = styled.div` 
+  display: flex; 
+  justify-content: center; 
+  align-items: center; 
+  background-image: url(${moneysImage});
+  background-attachment: fixed;
+  background-size: cover;
+  height: 100vh; 
+  width: 100vw;
+    background-position: center;
+    
+`; 
+  
+const App = () => { 
+  return ( 
+    <Main> 
+      <GlobalStyles /> 
+      <Tracker /> 
+    </Main> 
+  ) 
+} 
+  
 export default App;
